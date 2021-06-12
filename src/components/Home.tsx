7fcca12f63f808ee290b79d5fde6e773d211f26c
ChanhuiSeok <chanhuicom@gmail.com>
2021-06-12 22:05:23 +0900

    eslint, prettier 설정 및 tsx 파일 임시로 추가

---
+++ src/components/Home.tsx
@@ -0,0 +1,13 @@
+import {useState} from 'react';
+
+const Home = () => {
+    const [name, setName] = useState(null);
+
+    return (
+        <div>
+            test
+        </div>
+    );
+};
+
+export default Home;
