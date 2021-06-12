ff73d1c767ed090ce731d0b890a2d2d57f57d7d8
ChanhuiSeok <chanhuicom@gmail.com>
2021-06-13 00:21:10 +0900

    typescript용 styled-components 기본 설정(다크/라이트 모드 테스트)

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,21 +1,20 @@
 import {BrowserRouter as Router, Route} from 'react-router-dom';
-import {createGlobalStyle} from 'styled-components';
+import {ThemeProvider} from 'styled-components';
+import {GlobalStyles} from '../common/styles/globalStyles';
+import {lightTheme, darkTheme} from '../common/styles/themes';
 import Home from './Home';
 
-const GlobalStyles = createGlobalStyle`
-  body {
-    font-family: 'Noto Sans KR', sans-serif;
-    word-spacing:-1px;
-  }
-`;
-
-const App = () => (
-  <>
-    <GlobalStyles />
-    <Router>
-      <Route exact path="/" component={Home} />
-    </Router>
-  </>
-);
+const App = () => {
+  return (
+    <>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyles />
+        <Router>
+          <Route exact path="/" component={Home} />
+        </Router>
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default App;
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,8 +1,11 @@
 import {useState} from 'react';
+import styled from 'styled-components';
 
 /** 처음 진입하는 홈페이지 */
 const Home = () => {
   const [names, setNames] = useState(null);
+
+  const Title = styled.div``;
 
   return <div>테스트입니다.</div>;
 };
